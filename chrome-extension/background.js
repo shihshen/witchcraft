05e629e9bd5b4c6e8fba05d88a6b8e25e59e9997
shihshen <powpowfp@gmail.com>
2021-03-25 16:50:38 -0400

    remove google analytics and reduce numbers of requests.

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,9 +1,2 @@
-
-// bind it to window so it can be accessed from the popup screen
-if (typeof Analytics === "function") {  // will be undefined during tests
-    window.analytics = new Analytics();
-    window.analytics.send("Background", "Load");
-}
-
 // bind it to window so it can be accessed from the popup screen
 window.witchcraft = new Witchcraft(chrome, typeof document !== "undefined" ? document : undefined, window.analytics);
